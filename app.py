39fcc0f004356a804f6fbfadb2f67998cc5cfdd9
Andrew <59933938+andrewcampagnagit@users.noreply.github.com>
2023-05-26 10:41:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 h = {"User-ID":"andrew.campagna", "API-Key":"1rzZTSRNOsJoRm09QHt05fOXyiddJkxIPcoGxCoNSDw6VwFP"}
-p = [("host":os.environ["APP_INPUTS"]),("live":"true")]
+p = [("host",os.environ["APP_INPUTS"]),("live","true")]
 response = requests.get("https://neutrinoapi.net/domain-lookup", headers=h, params=p)
 
 print(response.content)
