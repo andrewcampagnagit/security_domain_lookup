42f08ba2b8e934f1521bd8efced078191993e5d6
Andrew <59933938+andrewcampagnagit@users.noreply.github.com>
2023-05-26 11:59:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 h = {"User-ID":"andrew.campagna", "API-Key":"1rzZTSRNOsJoRm09QHt05fOXyiddJkxIPcoGxCoNSDw6VwFP"}
 p = [("host",os.environ["APP_INPUTS"]),("live","true")]
 response = requests.get("https://neutrinoapi.net/domain-lookup", headers=h, params=p)
-
+response.json()["blocklists"]
 print(response.json())
