57bbb8a45028aab2658ce580d6de76e256c5147e
Andrew <59933938+andrewcampagnagit@users.noreply.github.com>
2023-05-31 00:09:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ import os
 h = {"User-ID":"andrew.campagna", "API-Key":"1rzZTSRNOsJoRm09QHt05fOXyiddJkxIPcoGxCoNSDw6VwFP"}
 p = [("host",os.environ["APP_INPUTS"]),("live","true")]
 response = requests.get("https://neutrinoapi.net/domain-lookup", headers=h, params=p)
-print(response.json())
+data = response.json()
+
+message = "This domain contains \n"
+
+for item in data["sensors"]:
+	message += "- "+ item["description"] +"\n"
+
+print(message)
