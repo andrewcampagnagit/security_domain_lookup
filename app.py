02ec2a28ed04a47494e3c956689a1df20f1972a5
Andrew <59933938+andrewcampagnagit@users.noreply.github.com>
2023-05-31 00:48:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,35 +2,7 @@ import requests
 import os
 
 h = {"User-ID":"andrew.campagna", "API-Key":"1rzZTSRNOsJoRm09QHt05fOXyiddJkxIPcoGxCoNSDw6VwFP"}
-
-def get_domain_info(dns_name):
-
-	p = [("host",dns_name),("live","true")]
-	response = requests.get("https://neutrinoapi.net/domain-lookup", 
-							headers=h, 
-							params=p)
-	data = response.json()
-	message = "This domain contains \n"
-	for item in data["sensors"]:
-		message += "- "+ item["description"] +"\n"
-	print(message)
-
-def get_ip_addr_info(ipv4_addr):
-
-	p = [("ip",ipv4_addr)]
-	response = requests.get("https://neutrinoapi.net/ip-blocklist", 
-							headers=h, 
-							params=p)
-	data = response.json()
-	message = "This IP address contains \n"
-	for item in data["sensors"]:
-		message += "- "+ item["description"] +"\n"
-	print(message)
-
-inputs = os.environ["APP_INPUTS"].split(",")
-
-if inputs[0] == "DNS Name":
-	get_domain_info(inputs[1])
-
-if inputs[0] == "IP Address":
-	get_ip_addr_info(inputs[1])
+p = [("host",os.environ["APP_INPUTS"]),("live","true")]
+response = requests.get("https://neutrinoapi.net/domain-lookup", headers=h, params=p)
+data = response.json()
+print(data)
